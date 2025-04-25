26e34b8bbea82e21fde45ebcba86bb1cc50adafb
Ayon Sarkar <ayonsarkar380@gmail.com>
2025-04-25 10:48:56 +0530

    v0.50.1

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import localFont from "next/font/local";
+import ThemeInitializer from '@/app/components/ThemeInitializer';
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -48,6 +49,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable}  ${geistMono.variable} font-google ${google.variable} antialiased`}
       >
+        <ThemeInitializer />
+        
         {children}
       </body>
     </html>
